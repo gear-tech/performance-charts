69ff9876918dd759c53da6c25c44ba9a3bf762c8
github-action-benchmark <github@users.noreply.github.com>
2022-09-14 23:31:22 +0000

    add pallet-gear extrinsic weights (customSmallerIsBetter) benchmark result for 8fc2990c4fdbd846a83ba4b105d27b5c1b8cd42e

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663198275900,
+  "lastUpdate": 1663198282598,
   "repoUrl": "https://github.com/gear-tech/gear",
   "entries": {
     "Gear workspace tests": [
@@ -15808,6 +15808,58 @@ window.BENCHMARK_DATA = {
           {
             "name": "upload_code",
             "value": 72,
+            "unit": "ns"
+          },
+          {
+            "name": "upload_program",
+            "value": 79,
+            "unit": "ns"
+          },
+          {
+            "name": "create_program",
+            "value": 3,
+            "unit": "ns"
+          },
+          {
+            "name": "send_message",
+            "value": 2,
+            "unit": "ns"
+          },
+          {
+            "name": "send_reply",
+            "value": 2,
+            "unit": "ns"
+          },
+          {
+            "name": "claim_value",
+            "value": 0,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "8fc2990c4fdbd846a83ba4b105d27b5c1b8cd42e",
+          "message": "Bump anyhow from 1.0.64 to 1.0.65 (#1490)",
+          "timestamp": "2022-09-14T22:34:57Z",
+          "url": "https://github.com/gear-tech/gear/commit/8fc2990c4fdbd846a83ba4b105d27b5c1b8cd42e"
+        },
+        "date": 1663198281883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "upload_code",
+            "value": 75,
             "unit": "ns"
           },
           {
